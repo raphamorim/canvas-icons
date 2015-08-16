62bcb915e598f3b2d059fe5eafcaf5499f4a2f68
Raphael H. Amorim <rapha850@gmail.com>
2015-08-16 18:41:21 -0300

    initial commit

---
+++ canvasIcons.js
@@ -0,0 +1,10 @@
+var CanvasIcons = (function() {
+    
+    this.init = function() {
+        console.log("Canvas Icons");
+    };
+
+    return {
+        init: this.init.bind(this)
+    }
+})();
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Canvas Icons
+
+## Why?
+
+Why render items with canvas?
+
+### Canvas Icons VS SVG Icons
+
+### Canvas Icons VS Image Icons
+
+
+## Usage

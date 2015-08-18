44a0fd7685a0a89dd40f1214893d4f23ffe20a21
Raphael H. Amorim <rapha850@gmail.com>
2015-08-18 20:12:32 -0300

    capture prefix canvas-icon

--- canvasIcons.js
+++ canvasIcons.js
@@ -1,11 +1,32 @@
 var canvasIcons = (function() {   
     this.init = function() {
-        this.icons = document.querySelectorAll('.canvas-icon');
-        console.log(icons);
+        this.elementList = [];
+        this.getElements();
+        this.generateCanvas();
+        console.log(this.elementList)
     };
 
-    this.draw = function() {
-
+    this.getElements = function() {
+        this.elementList.forEach.call(document.getElementsByTagName('div'), function(element) {
+            (function(names, i) {
+                while (i < names.length) {
+                    var name = names[i];
+                    if (name.indexOf('canvas-icon-') === 0) {
+                        names.remove(name);
+                        this.elementList.push(name);
+                    } else {
+                        ++i;
+                    }
+                }
+            }(element.classList, 0));
+        });
+    };
+    this.generateCanvas = function() {
+        var itens = this.elementList;
+        for (var index = 0; index < itens.length; index++) {
+            // TODO: Use a createChildNode and append
+            itens[index].innerHTML = '<canvas class="' + itens[index] + '"></class>'
+        }
     };
 
     return {
